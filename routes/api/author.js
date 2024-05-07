ed657131a91696645dc2090e9d849b4b8a67499b
sajeed11 <sajednemli39@gmail.com>
2024-05-07 23:53:06 +0100

    Add read genre bu the Author

--- routes/api/author.js
+++ routes/api/author.js
@@ -3,6 +3,7 @@ import express from 'express';
 // Controllers
 import bookController from '../../controllers/bookController.js';
 import authController from '../../controllers/authController.js';
+import genreController from '../../controllers/genreController.js';
 import authorRequestController from '../../controllers/authorRequestController.js';
 
 // Middlewares
@@ -29,7 +30,12 @@ router.post('/books', authMiddleware, uploadFile.single('picture'), bookControll
 router.put('/books/:id', authMiddleware, bookController.updateBook)
 router.post('/books/:id', authMiddleware, bookController.deleteBook)
 
+// Author with Genres
+router.get('/genres', authMiddleware, genreController.readAll)
+router.get('/genres/:id', authMiddleware, genreController.readById)
+
 // Author with his request
 router.get('/author_requests/:id', authMiddleware, authorRequestController.read)
+router.delete('/author_requests/:id', authMiddleware, authorRequestController.delete)
 
 export default router;
