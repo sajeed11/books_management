00c5f9c18add9e80b8b42b0dad07aabaed87c216
Sadjed <77120200+sajeed11@users.noreply.github.com>
2024-06-22 18:20:59 +0100

    Update uploadFiles.js
    Removing useless comment

--- middlewares/uploadFiles.js
+++ middlewares/uploadFiles.js
@@ -2,33 +2,6 @@ import multer from "multer";
 import fs from 'fs';
 import path from 'path';
 
-// const pathv = '../uploads';
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const dir = pathv;
-
-//     fs.exists(dir, (exist) => {
-//       if (!exist) {
-//         return fs.mkdir(dir, (error) => cb(error, dir));
-//       }
-//       return cb(null, dir);
-//     });
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     cb(null, file.fieldname + '-' + uniqueSuffix)
-//   },
-// });
-
-
-// export const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5,
-//   },
-// });
-
 export const uploadFile =
   multer({
     storage: multer.diskStorage({
